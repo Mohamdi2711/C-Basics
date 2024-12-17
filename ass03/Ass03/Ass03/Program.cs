a01e2c1189bfed95bac154c8bda57971d2a181f3
mohamed hamdi <mohamdi111200444@gmail.com>
2024-12-17 16:32:59 +0200

    Q.1

---
+++ ass03/Ass03/Ass03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ass03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            //Console.Write("Plz Enter The Number : ");
+            //int Number = int.Parse(Console.ReadLine());
+            //if ( Number % 4 == 0 && Number % 3 == 0)
+            //{
+            //    Console.WriteLine("YES");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NO");
+            //}
+            #endregion
+
+
+        }
+    }
+}
